142d7a817127edc7bcc3dbfe4673634742204732
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2019-03-13 19:48:03 -0400

    first commit

---
+++ ImgApp/Pagethree.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ImgApp
+{
+    public partial class Pagethree : ContentPage
+    {
+        public Pagethree()
+        {
+            InitializeComponent();
+        }
+    }
+}
